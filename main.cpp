80f22067ba215fb8a9473e5bca58ac508411d406
LiWinDom <wadim.gorbachev@mail.ru>
2024-10-28 16:02:49 +0300

    Added finding solution with perebor

--- main.cpp
+++ main.cpp
@@ -35,5 +35,23 @@ int main() {
       break;
   }
 
+  // Perebor
+  double ans = 0;
+  for (auto i = 0; i < 10; ++i) {
+    for (auto j = 0; j < 10; ++j) {
+      for (auto k = 0; k < 10; ++k) {
+        for (auto l = 0; l < statement.bVector.size(); ++l) {
+          if (i * statement.aMatrix[l][0] + j * statement.aMatrix[l][1] + k * statement.aMatrix[l][2] > statement.bVector[l]) {
+            goto next;
+          }
+        }
+        // Every statement is correct
+        ans = std::max(i * statement.functionVector[0] + j * statement.functionVector[1] + k * statement.functionVector[2], ans);
+        next:
+      }
+    }
+  }
+  std::cout << std::endl << "Answer with perebor: " << ans << std::endl;
+
   return 0;
 }
